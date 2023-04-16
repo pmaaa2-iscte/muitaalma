0b15a53afd6af8cdd5af0067baff005b08cf20a9
pmaaa2-iscte <pmaaa2@iscte-iul.pt>
2023-04-16 16:02:47 +0100

    first coomit

---
+++ ola_projecto/src/main/java/pmaaa2_iscte/ola_projecto/App.java
@@ -0,0 +1,13 @@
+package pmaaa2_iscte.ola_projecto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
